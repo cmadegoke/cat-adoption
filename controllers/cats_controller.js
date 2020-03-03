4b055b39f31ee6ee2fb763195a91b500f99452bd
Catherine Adegoke <cmadegoke@gmail.com>
2020-03-02 21:44:10 -0500

    complete code

--- controllers/cats_controller.js
+++ controllers/cats_controller.js
@@ -1,67 +1,68 @@
 const connection = require('../config/connection');
 
+// GET ALL CATS
 const getCats = () => {
-    return new Promise((reslove, reject) => {
-        connection.query('SELECT * FROM cats', (err, catdata) => {
-            if (err) {
-                consola.error(err);
-                return reject(err);
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM cats', (err, catdata) => {
+      if (err) {
+        console.log(err);
+        // THIS WILL GO TO PROMISE'S .catch()
+        return reject(err);
+      }
 
-            }
-            resolve({ catdata });
-
-        })
+      // THIS WILL GO TO PROMISE'S .then()
+      resolve(catdata);
     });
+  });
 };
 
-    const createCat = catObj => {
-        return new Promise((resolve, reject) => {
-            connection.query('INSERT INTO items SET ?', catObj, (err, catdata) => {
-                if (err) {
-                    consola.error(err);
-                    return reject(err);
-
-                }
-                resolve({ catdata });
-            });
-
-        });
-    };
-
-    const updateCat = (catObj, catId) => {
-        return new Promise((resolve, reject) => {
-          
-          connection.query(
-            'UPDATE items SET ? WHERE ?',
-            [catObj,catId],
-            (err, catdata) => {
-              if (err) {
-                consola.error(err);
-                reject(err);
-                return;
-              }
-              resolve({ message: 'cat updated' });
-            }
-          );
-      
-        
-        });
-      };
-const deleteCat= (catId) => {
-    return new Promise((resolve, reject) => {
-    connection.query( 'DELETE FROM cats  WHERE  id = ?',[catId],
-(err, catdata) => {
-            if (err) {
-              console.log(err);
-              reject(err);
-              return;
-            }
-            resolve({ catdata});
-          }
-        );
-    
-      
-      });
+// create a cat
+/* accepts object parameter => {cat_name: "Mr. Mustaphales" } */
+const createCat = catObj => {
+  return new Promise((resolve, reject) => {
+    connection.query('INSERT INTO cats SET ?', catObj, (err, catdata) => {
+      if (err) {
+        console.log(err);
+        // THIS WILL GO TO PROMISE'S .catch()
+        return reject(err);
+      }
+      // THIS WILL GO TO PROMISE'S .then()
+      resolve(catdata);
+    });
+  });
 };
 
-module.exports = {getCats,updateCat,createCat,deleteCat};
+// UPDATE A CAT'S ADOPTION STATUS
+// catObj => { adopted: true } OR {adopted: false}
+const updateCat = (catObj, catId) => {
+  return new Promise((resolve, reject) => {
+    connection.query('UPDATE cats SET ? WHERE id = ?', [catObj, catId], (err, catdata) => {
+      if (err) {
+        console.log(err);
+        return reject(err);
+      } else if (catdata.affectedRows === 0) {
+        return resolve({ message: "Couldn't find a cat with that id!", code: 404 });
+      }
+
+      resolve({ message: 'Cat updated successfully!', code: 200 });
+    });
+  });
+};
+
+// DELETE A CAT
+const deleteCat = catId => {
+  return new Promise((resolve, reject) => {
+    connection.query('DELETE FROM cats WHERE id = ?', [catId], (err, catdata) => {
+      if (err) {
+        console.log(err);
+        return reject(err);
+      } else if (catdata.affectedRows === 0) {
+        return resolve({ message: "Couldn't find a cat with that id!", code: 404 });
+      }
+
+      resolve({ message: 'Cat deleted successfully!', code: 200 });
+    });
+  });
+};
+
+module.exports = { getCats, createCat, updateCat, deleteCat };
--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,21 @@
-DROP DATABASE IF EXISTS cats_db
+DROP DATABASE IF EXISTS cats_db;
 
-CREATE DATABASE cats_db
+CREATE DATABASE cats_db;
 
-USE cats_db
+USE cats_db;
 
-CREATE TABLE cats(
-    id INT NOT NULL AUTO INCREMENT,
-    cat_name VARCHAR (225) NOT NULL,
-    adopted BOOLEAN DEFAULT false NOT NULL,
-    PRIMARY KEY (id)
-
+CREATE TABLE cats (
+  id INT NOT NULL AUTO_INCREMENT,
+  cat_name VARCHAR(255) NOT NULL,
+  adopted BOOLEAN DEFAULT false NOT NULL,
+  PRIMARY KEY (id)
 );
 
-INSERT INTO cats 
-(cat_nam)
-VALUES 
-('jasper')
-('Hope')
-('James')
+INSERT INTO cats
+  (cat_name)
+VALUES
+  ('Macaroni'),
+  ('Mr. Fluffypants'),
+  ('Kevin');
 
-SELECT * FROM cats
+SELECT * FROM cats;
